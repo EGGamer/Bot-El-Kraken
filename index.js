8c3f244198ea2a2df7117980823401cf44fc0fdf
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-15 17:31:00 +0200

    xd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const fs = requiere("fs");
+const fs = require("fs");
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) =>{
