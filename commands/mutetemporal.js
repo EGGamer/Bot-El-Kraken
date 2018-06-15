3243c3387e27efec6227efea89f12c8e282a004a
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-15 18:30:23 +0200

    xd

--- commands/mutetemporal.js
+++ commands/mutetemporal.js
@@ -7,36 +7,18 @@ module.exports.run = async (bot, message, args) => {
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("No se puede mutearle!");
   let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
+  
   let mutetime = args[1];
-  if(!mutetime) return message.reply("You didn't specify a time!");
+  if(!mutetime) return message.reply("No has especificado un tiempo!");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> ha sido muteado por ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`<@${tomute.id}> ha sido desmuteado!`);
   }, ms(mutetime));
 
 
