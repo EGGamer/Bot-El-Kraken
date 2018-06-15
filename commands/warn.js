96356994c81007170e519ec36fa59da83bb8c450
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-15 21:38:58 +0200

    warn

--- commands/warn.js
+++ commands/warn.js
@@ -3,62 +3,72 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-module.exports.run = async (bot, message, args) =>
-{
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No puedo hacerlo!");
-    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if(!wUser) return message.reply("No he encontrado ese usuario");
-    if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("No puedo hacerlo");
-    let reason = args.join(" ").slice(22);
+module.exports.run = async (bot, message, args) => {
 
-    if(!warns[wUser.id]) warns[wUser.id] =
-    {
-        warns: 0
-    };
+  //!warn @daeshan <reason>
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No puedo hacerlo!");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("No he podido encontrar al usuario");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("No puedo hacer eso!");
+  let reason = args.join(" ").slice(22);
 
-    warns[wUser.id].warns++;
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
 
-    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
-        if (err) console.log(err);
-    });
+  warns[wUser.id].warns++;
 
-    let warnEmbed = new Discord.RichEmbed()
-    .setDescription("Warnings")
-    .setColor("#f4b942")
-    .addField("Usuario avisado", `<@${wUser.tag}>`)
-    .addField("Avisado en", message.channel)
-    .addField("Cantidad de Avisos", warns[wUser.id].warns)
-    .addField("razón", reason);
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+  });
 
+  let warnEmbed = new Discord.RichEmbed()
+  .setDescription("Avisos")
+  .setAuthor(message.author.username)
+  .setColor("#fc6400")
+  .addField("Usuario avisado", `<@${wUser.id}>`)
+  .addField("Avisado en", message.channel)
+  .addField("Cantidad de avisos", warns[wUser.id].warns)
+  .addField("Razón", reason);
 
-    let warnchannel = message.guild.channel.find(`name`, "incidentes");
+  let warnchannel = message.guild.channels.find(`name`, "incidentes");
+  if(!warnchannel) return message.reply("No he podido encontrar el canal incidentes");
 
-    if(!warnchannel) return message.reply("no he podido encontrar el canal de incidentes");
+  warnchannel.send(warnEmbed);
 
-    warnchannel.send(warnEmbed);
+  if(warns[wUser.id].warns == 1){
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole) return message.reply("Deberías crear el rol de muted");
 
-    if(warns[wUser.id].warns == 2)
-    {
-        let muterole = message.guild.roles.find(`name`, "muted");
-        if(!muterole) return message.reply("Deberías crear el rol de muted");
-        
-        let mutetime = "5m";
-        await(wUser.addRole(muterole.id));
-        message.channel.send(`${wUser.tag} ha sido muteado por 5 minutos`);
+    let mutetime = "5m";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`<@${wUser.id}> ha sido muteado por 1 minutos`);
 
-        setTimeout(function(){
-            wUser.removeRole(muterol.id);
-            message.channel.reply(`${wUser} ha sido desmuteado`);
-        }), ms(mutetime);
-    }
-    if(warns[wUser.id].warns == 3)
-    {
-        message.guild.member(wUser).kick(reason);
-        message.channel.send(`${wUser} ha sido expulsado por ${reason}`);
-    }
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.reply(`<@${wUser.id}> ha sido desmuteado`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 1){
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole) return message.reply("Deberías crear el rol de muted");
+
+    let mutetime = "10m";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`<@${wUser.id}> ha sido muteado por 10 minutos`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.reply(`<@${wUser.id}> ha sido desmuteado`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 3){
+    message.guild.member(wUser).kick(reason);
+    message.reply(`<@${wUser.id}> ha sido expulsado.`)
+  }
+
 }
 
-module.exports.help = 
-{
-    name: "warn"
+module.exports.help = {
+  name: "warn"
 }
