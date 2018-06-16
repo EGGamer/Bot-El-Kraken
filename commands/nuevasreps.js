59ad73897570d72e6f522e172b03a3225af325f1
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-16 14:02:00 +0200

    xd

--- commands/nuevasreps.js
+++ commands/nuevasreps.js
@@ -9,8 +9,8 @@ module.exports.run = async (bot, message, args) =>
         .addField("EG Gamer", "**38**, **39**, **38**, **2**")
         .addField("Hyren", "**38**, **39**, **38**, **2**")
         .addField("Fran", "**30**, **28**, **27**, **1**")
-        .addField("Carlis", "**?**, **?**, **?**, **?**")
-        .addField("Cutu", "**?**, **?**, **?**, **?**")
+        .addField("Carlis", "**?**, **?**, **?**, **0**")
+        .addField("Cutu", "**22**, **21**, *17?**, **0**")
         .setImage("https://i.imgur.com/ypara7v.png");
         //message.channel.send(repsEmbed);
     
