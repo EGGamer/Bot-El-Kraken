15b32b8f6f52138baa08cec2911f001ec08f8838
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-16 13:04:25 +0200

    say

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>
 {
-    
+    if(message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nmope");
+    let botmessage = args.join(" ");
+    message.delete().catch();
+    message.channel.send(botmessage);
+
 }
 
 module.exports.help = {
