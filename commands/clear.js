48c2620d55a50156df074788c617114ee3b60888
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-16 12:57:06 +0200

    clear

---
+++ commands/clear.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>
+{
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nope...");
+    if(!args[0]) return message.channel.send("¡Debes poner cuántos mensages quieres que elimine!");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Eliminados ${args[0]} mensajes`).then(msg => msg.delete(5000));
+    });
+}
+
+module.exports.help = {
+    name: "clear"
+}
