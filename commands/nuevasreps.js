7568258a6aa5b94a4825276e1221d21796355a5a
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-16 12:02:25 +0200

    xd

--- commands/nuevasreps.js
+++ commands/nuevasreps.js
@@ -4,18 +4,18 @@ module.exports.run = async (bot, message, args) =>
 {
     
         var repsEmbed = new Discord.RichEmbed()
-        embed.setColor(0x39efbf);
-        embed.setTitle("Reputaciones Actuales");
-        embed.addField("EG Gamer", "**38**, **39**, **38**, **2**");
-        embed.addField("Hyren", "**38**, **39**, **38**, **2**");
-        embed.addField("Fran", "**30**, **28**, **27**, **1**");
-        embed.addField("Carlis", "**?**, **?**, **?**, **?**");
-        embed.addField("Cutu", "**?**, **?**, **?**, **?**");
-        embed.setImage("https://i.imgur.com/ypara7v.png");
-        message.channel.send(repsEmbed);
+        .setColor("#39efbf")
+        .setTitle("Reputaciones Actuales")
+        .addField("EG Gamer", "**38**, **39**, **38**, **2**")
+        .addField("Hyren", "**38**, **39**, **38**, **2**")
+        .addField("Fran", "**30**, **28**, **27**, **1**")
+        .addField("Carlis", "**?**, **?**, **?**, **?**")
+        .addField("Cutu", "**?**, **?**, **?**, **?**")
+        .setImage("https://i.imgur.com/ypara7v.png");
+        //message.channel.send(repsEmbed);
     
         let repsChannel = message.guild.channels.find(`name`, "reputaciones-actuales");
-    if(!incidentsChannel) return message.channel.send("No he podido encontrar el canal de reputaciones");
+    if(!repsChannel) return message.channel.send("No he podido encontrar el canal de reputaciones");
     
     repsChannel.send(repsEmbed);
 }
