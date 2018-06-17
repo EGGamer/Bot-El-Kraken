c798e5d83a24e295e6913f6371f9c83f6908c0f6
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 12:34:11 +0200

    xd

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/administracion", (err, files) =>{
+fs.readdir("./commands/", (err, files) =>{
     if(err) console.log(err);
 
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if(jsfile.length <= 0){
-        conslo.log("No se han podido encontar comandos");
+        conslo.log("No se hna podido encontar comandos");
         return;
     }
 
@@ -20,48 +20,13 @@ fs.readdir("./commands/administracion", (err, files) =>{
     bot.commands.set(props.help.name, props);
 })
 
-});
-fs.readdir("./commands/ayudabot", (err, files) =>{
-    if(err) console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0){
-        conslo.log("No se han podido encontar comandos");
-        return;
-    }
-
-    jsfile.forEach((f, i) =>
-{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`)
-    bot.commands.set(props.help.name, props);
 })
-
-});
-fs.readdir("./commands/reputaciones", (err, files) =>{
-    if(err) console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0){
-        conslo.log("No se han podido encontar comandos");
-        return;
-    }
-
-    jsfile.forEach((f, i) =>
-{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`)
-    bot.commands.set(props.help.name, props);
-})
-
-});
-
 
 
 
 bot.on("ready", async =>{
     console.log(`${bot.user.username} está online!`);
-    bot.user.setActivity("LOS KRUKEN CHANCLAS", {type: "LISTENING"});
+    bot.user.setActivity("LOS KRUKEN CHANCLAS", {type: "WATCHING"});
     
     //bot.user.setActivity("-kayuda | Bot Oficial de LOS KRUKEN CHANCLAS. ");
 });
