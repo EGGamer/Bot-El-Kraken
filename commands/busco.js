cdba092b9fafed48e94671accb830a61dbab3cae
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 18:40:03 +0200

    xd

--- commands/busco.js
+++ commands/busco.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message, args) =>
 {
     //-busco <juego>
    let juego = args[0];
-   let juegos = ["dbd", "sot", "rl", "ow", "ft"];
+   
    
    if(!juego) return message.reply(` no has especificado un juego.`);
-   if(!juego == juegos) return message.reply("Pon un juego correcto!");
+   if(!juego == "dbd" || "sot" || "rl" || "ow" || "ft") return message.reply("Pon un juego correcto!");
    if(juego === "dbd") return message.reply(` está buscando gente para jugar **Dead by Daylight**`);
    if(juego === "sot") return message.reply(` está buscando gente para jugar **Sea of Thieves**`);
    if(juego === "rl") return message.reply(` está buscando gente para jugar **Rocket League**`);
