c245f93dc3519f37e4719e6b1859d215510f58c1
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 18:01:27 +0200

    test busco

--- commands/busco.js
+++ commands/busco.js
@@ -3,8 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) =>
 {
     //-busco <juego>
-   // let juego = message.args[0];
-    message.channel.send(args[0]);
+   let juego = message.args[0];
+   
+   if(!juego) return message.reply(`${message.author} no has especificado un juego.`);
+   
+   if(juego) return message.reply(`${message.author} estña buscando gente para jugar ${juego}.`);
+   message.channel.send(args[0]);
+
+    
 }
 
 module.exports.help = {
