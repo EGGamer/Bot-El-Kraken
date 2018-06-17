57abd2389e0ba9c70be799ed7e90215ea23d78be
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 20:00:27 +0200

    arreglo en prefix?

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on("message", async message =>
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    let commandfile = bot.commands.get(cmd.slice(prefix));
     if(commandfile) commandfile.run(bot, message, args);
 
  
