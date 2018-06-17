4152bc966ac1577edf205157dbfa7c6e1fcd2db1
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 18:45:34 +0200

    mensajes en respectivos canales

--- commands/busco.js
+++ commands/busco.js
@@ -5,16 +5,22 @@ module.exports.run = async (bot, message, args) =>
     //-busco <juego>
    let juego = args[0];
    
+   //Canales de texto
+   let bgSot = message.guild.channels.find(`name`, "busco-gente-sot");
+   let bgDbd = message.guild.channels.find(`name`, "busco-gente-dbd");
+   let bgRl = message.guild.channels.find(`name`, "busco-gente-rl");
+   let bgOw = message.guild.channels.find(`name`, "busco-gente-ow");
+   let bgFt = message.guild.channels.find(`name`, "busco-gente-ft");
    
    if(!juego) return message.reply(` no has especificado un juego.`);
-   if(!juego == "dbd" || "sot" || "rl" || "ow" || "ft") return message.reply("Pon un juego correcto!");
-   if(juego === "dbd") return message.reply(` está buscando gente para jugar **Dead by Daylight**`);
-   if(juego === "sot") return message.reply(` está buscando gente para jugar **Sea of Thieves**`);
-   if(juego === "rl") return message.reply(` está buscando gente para jugar **Rocket League**`);
-   if(juego === "ft") return message.reply(` está buscando gente para jugar **Fortnite**`);
-   if(juego === "ow") return message.reply(` está buscando gente para jugar **Overwatch**`);
    
-   //if(juego) return message.channel.send(`@everyone <@${message.author.id}> está buscando gente para jugar **${juego}**.`);
+   if(juego === "dbd") return message.bgDbd.send(`${message.author} está buscando gente para jugar **Dead by Daylight**`);
+   if(juego === "sot") return message.bgSot.send(`${message.author} está buscando gente para jugar **Sea of Thieves**`);
+   if(juego === "rl") return message.bgRl.send(`${message.author} está buscando gente para jugar **Rocket League**`);
+   if(juego === "ft") return message.bgFt.send(`${message.author} está buscando gente para jugar **Fortnite**`);
+   if(juego === "ow") return message.bgOw.send(`${message.author} está buscando gente para jugar **Overwatch**`);
+   
+   
 
 
     
