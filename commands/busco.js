b9b07451d2c446fc965c86d209ecf7811d42cc1f
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 17:54:16 +0200

    busco version test

--- commands/busco.js
+++ commands/busco.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) =>
 {
     //-busco <juego>
-    let juego = message.args[0];
-    message.channel.send(juego);
+   // let juego = message.args[0];
+    message.channel.send(args[0]);
 }
 
 module.exports.help = {
