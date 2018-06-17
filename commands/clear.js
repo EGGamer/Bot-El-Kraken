639d89f9c85d6c315692694e55b48f14b2386852
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-17 12:16:30 +0200

    xd

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) =>
     if(!args[0]) return message.channel.send("¡Debes poner cuántos mensages quieres que elimine!");
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`Eliminados ${args[0]} mensajes`).then(msg => msg.delete(5000));
-    });
+    }); 
 }
 
 module.exports.help = {
