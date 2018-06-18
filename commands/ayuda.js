34a177dee20a53b9ade4a35b50097f58bba5a1e6
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-18 15:17:53 +0200

    ayuda prototipo

---
+++ commands/ayuda.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>
+{
+    message.channel.send(`<@${message.author.id}> aquí tienes todo lo que puedo hacer:`);
+    let ayudaEmbed = new Discord.RichEmbed()
+    .setTitle("¿NECESITAS AYUDA?")
+    .setDescription("Aquí tienes toda la información que necesitas saber sobre el bot. (En desarrollo, esto va a cambiar)")
+    .addField("COMANDOS", ":", true)
+}
+
+module.exports.help = {
+    name: "info"
+}
