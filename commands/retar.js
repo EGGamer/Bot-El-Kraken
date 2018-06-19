5e424218b7aad6aac2f7df52292ca167dc78c65d
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-19 18:26:55 +0200

    juegos

--- commands/retar.js
+++ commands/retar.js
@@ -5,8 +5,14 @@ module.exports.run = async (bot, message, args) =>
     let founder = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let juego = args.join(" ").slice(22)
     let founderRole = message.guild.roles.find(`name`, "LKC Founder");
-    //si no es founder
+    //Si no es founder
     if(!founder.roles.has(founderRole.id)) return message.reply("debes retar a un LKC Founder");
+    //Si no es un juego elegible.
+    if(!juego === "dbd") return message.reply("debes elegir un juego correcto.");
+    if(!juego === "sot") return message.reply("debes elegir un juego correcto.");
+    if(!juego === "rl") return message.reply("debes elegir un juego correcto.");
+    if(!juego === "ow") return message.reply("debes elegir un juego correcto.");
+    if(!juego === "ft") return message.reply("debes elegir un juego correcto.");
     
     let retosChannel = message.guild.channels.find(`name`, "retadores-lkc");
     if(!retosChannel) return message.channel.send("No he podido encontrar el canal de retos");
