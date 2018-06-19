21a54df8e2ae89bdccc85e03a960a4c5e1118cf6
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-19 17:48:12 +0200

    test

--- commands/retar.js
+++ commands/retar.js
@@ -2,16 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>
 {
-    let founder = message.guild.member(message.mentions.users.id.first() || message.guild.members.get(args[0]));
+    let founder = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let juego = args.join(" ").slice(22)
 
     //si no es founder
-    300537378991505418
-    if(!founder === "300537378991505418") return message.reply(" debes mencionar a un LKC Founder");
-    if(!founder === "267265766028476417") return message.reply(" debes mencionar a un LKC Founder");
-    if(!founder === "220952547630907394") return message.reply(" debes mencionar a un LKC Founder");
-    if(!founder === "256174955329355777") return message.reply(" debes mencionar a un LKC Founder");
-    if(!founder === "226373686309945344") return message.reply(" debes mencionar a un LKC Founder");
+    if(!founder.guild.roles.find(`name`, "LKC Founder")) return message.reply("Debes retar a un LKC Founder!")
 
     let retosChannel = message.guild.channels.find(`name`, "retadores-lkc");
     if(!retosChannel) return message.channel.send("No he podido encontrar el canal de retos");
