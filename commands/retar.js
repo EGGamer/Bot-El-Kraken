832a0498b40281fa4efff8d0319c0d46e9f1b670
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-20 09:07:48 +0200

    retar overwatch

--- commands/retar.js
+++ commands/retar.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) =>
         let retarDbDEmbed = new Discord.RichEmbed()
         .setTitle("¡Alguien ha retado a un LKC Founder!")
         .setDescription("Si el LKC Founder acepta el reto siempre y cuando tenga Dead by Daylight se acordará una fecha para hacer la partida.")
-        .addField("LA PARTIDA Y CÓMO GANAR", "La partida será privada. El founder Killer, y el que ha retado survivor (si los dos están de acuerdo se pueden cambiar los roles). Si el survivor escapa gana en cambio si muere pierde.")
+        .addField("LA PARTIDA Y CÓMO GANAR", "La partida será privada. El founder Killer, y el que ha retado survivor (si los dos están de acuerdo se pueden cambiar los roles). El retador puede llamar a 3 amigos suyo a que le ayuden en la partida, como una premade.. Si el retador escapa gana en cambio si muere pierde.")
         .addField("REGLAS", "No perks, ni add-ons, sólo pura habilidad. [Sujeto a Cambios]")
         .addField("PREMIO", "Si el que reta gana, se le otorgará un rol de haber ganado a un LKC Founder.")
         .addField("RETADOR:", `<@${message.author.id}>`, true)
@@ -57,7 +57,21 @@ module.exports.run = async (bot, message, args) =>
         retosChannel.send(retarRlEmbed);
 
     };
-    if(!juego === "ow") return message.reply("debes elegir un juego correcto.");
+    if(juego === "ow"){
+        let retarOwEmbed = new Discord.RichEmbed()
+        .setTitle("¡Alguien ha retado a un LKC Founder!")
+        .setDescription("Si el LKC Founder acepta el reto siempre y cuando tenga Overwatch se acordará una fecha para hacer la partida.")
+        .addField("LA PARTIDA Y CÓMO GANAR", "[*EN DESARROLLO*]]")
+        .addField("REGLAS", "[*EN DESARROLLO*]")
+        .addField("PREMIO", "Si el que reta gana, se le otorgará un rol de haber ganado a un LKC Founder.")
+        .addField("RETADOR:", `<@${message.author.id}>`, true)
+        .addField("LKC FOUNDER RETADO:", `${founder}`, true)
+        .setTimestamp()
+        .setThumbnail("https://bsmknighterrant.org/wp-content/uploads/2018/05/Fortnite-logo.png")
+        .setColor("#ff8300");
+        retosChannel.send(retarFtEmbed);
+
+    };
     if(juego === "ft"){
         let retarFtEmbed = new Discord.RichEmbed()
         .setTitle("¡Alguien ha retado a un LKC Founder!")
@@ -68,9 +82,9 @@ module.exports.run = async (bot, message, args) =>
         .addField("RETADOR:", `<@${message.author.id}>`, true)
         .addField("LKC FOUNDER RETADO:", `${founder}`, true)
         .setTimestamp()
-        .setThumbnail("https://bsmknighterrant.org/wp-content/uploads/2018/05/Fortnite-logo.png")
+        .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/2000px-Overwatch_circle_logo.svg.png")
         .setColor("#7e00fc");
-        retosChannel.send(retarFtEmbed);
+        retosChannel.send(retarOwEmbed);
 
     };
     if(juego === "lol"){
