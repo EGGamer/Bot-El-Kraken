0c60d14511567d71591ac29051ba67aa7112fd89
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-21 10:03:59 +0200

    bug fix?

--- commands/botOperativo.js
+++ commands/botOperativo.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) =>
     let author = message.member;
     let founderRole = message.guild.roles.find(`name`, "LKC Founder");
 
-    if(!author.roles.has(founderRole)) return message.reply("¡No tienes permiso para ejecutar ese comando!");
-    if(author.roles.has(founderRole)){
+    if(!author.roles.has(founderRole.id)) return message.reply("¡No tienes permiso para ejecutar ese comando!");
+    if(author.roles.has(founderRole.id)){
     let anunciosChannel = message.guild.channels.find(`name`, "anuncios-server");
     let embed = new Discord.RichEmbed()
     .setColor("#59ff3f")
