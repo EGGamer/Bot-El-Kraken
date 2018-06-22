5305d4d5147cbde9a1ea26a3e3bf4776dd2d2cc8
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-22 15:13:47 +0200

    embed update

--- commands/avisoTwitch.js
+++ commands/avisoTwitch.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) =>
 { 
     //k-avisoTwitch <usuario> <juego>|| Usuario: Carlis, EG
     let usuario = args[0];
-    let juego = args[1];
+    let juego = args.join(" ").slice(22);
     console.log(usuario);
     console.log(juego);
 
@@ -18,7 +18,9 @@ module.exports.run = async (bot, message, args) =>
         .setDescription(`¡Carlis104 está en directo en Twitch jugando ${juego}, id a verlo!`)
         .setThumbnail("https://static-cdn.jtvnw.net/jtv_user_pictures/2dc2b681-9b18-4640-939c-39e404121508-profile_image-70x70.png")
         .setAuthor("Carlis104", "https://cdn.discordapp.com/avatars/226373686309945344/60c4f41e766fbeff90af94a8b9861e64.png")
-        .setColor("#6441A4");
+        .setColor("#6441A4")
+        .setTimestamp()
+        .setFooter("Twitch.tv", "http://www.freelogovectors.net/wp-content/uploads/2016/12/twitch-logo1.png");
         twitchChannel.send(carlisTwEmbed);
     }
 }
