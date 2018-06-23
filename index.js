ffe40c82bfb6eb0dc43375ea44fa16a3989539cf
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-23 18:46:47 +0200

    test

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ bot.on("message", async message => {
     let prefix = botconfig.prefix; 
 
     let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+    let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
   
