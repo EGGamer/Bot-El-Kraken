a95522155c47e00c49da6dcb8a9e18ac64c91c8b
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-23 10:12:58 +0200

    sugerencia

---
+++ commands/sugerencia.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>
+{
+    //k-sugerir <Sugerencia>
+    let sugerencia = args.join(" ").slice(22);
+    let sugChannel = message.guild.channels.find(`name`, "sugerencias");
+    
+    if(!sugerencia) return message.reply("¡Debes describir la sugerencia!");
+    
+    let sugEmbed = new Discord.RichEmbed()
+    .setTitle("SUGERENCIA")
+    .setDescription(`¡<@${message.author.id}> ha enviado una sugerencia!`)
+    .addField("Sugerencia:", sugerencia)
+    .setThumbnail(message.guild.iconURL)
+    .setAuthor(message.member, message.member.avatarURL)
+    .setFooter(`ID: ${message.author.id}`)
+    .setTimestamp();
+
+    sugChannel.send(sugEmbed);
+}
+module.exports.help = {
+    name: "sugerir"
+}
