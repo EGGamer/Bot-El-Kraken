9f9203fae10044b9a77d9612eba73527972240cb
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-24 11:53:18 +0200

    pong

--- commands/pong.js
+++ commands/pong.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>
 {
-    message.author.sendMessgae("¿Ping?");
+    message.author.sendMessage("¿Ping?");
 
 }
 module.exports.help = {
