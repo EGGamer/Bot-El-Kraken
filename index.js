01f750535556c3ec1946a60371b309a6246976b3
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-27 17:01:45 +0200

    arreglos e eliminado lo innecesario

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ fs.readdir("./commands/", (err, files) =>{
 
 });
 
-bot.on("guildMemberAdd", (server, user) => {
-    user.sendMessage(`¡${user} bienvenido a ${server.name}! Soy El Kraken, el bot supremo del servidor. Te recomiendo leer este documento: http://bit.ly/lkcserver . Ahí encontrarrás la información completa sobre el servidor. también lo encontrarás en el canal #información del servidor. Esperamos que disfrutes del servidor. 😉`);
-});
-
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
