4c4d2d5d271722de3cfa89a5663470ec4074ca4a
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-28 13:24:22 +0200

    posible arreglo?

--- commands/suicidio.js
+++ commands/suicidio.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
+const config = require("./botconfig.json");
 
 module.exports.run = async (bot, message, args) =>
 {
     //k-suicidar <Nota de suicidio>
-    let nota = message.content;
+    let nota = args.slice(config.prefix);
 
     if(!nota) return message.channel.send(`¡<@${message.author.id}> se ha suicidado 😮! DEP 💀 <@${message.author.id}> 💀`);
     if(nota) return message.channel.send(`¡<@${message.author.id}> se ha suicidado 😮!` +  " Nos ha dejado una nota: " + `**${nota}**` + ` DEP 💀 <@${message.author.id}> 💀`);
