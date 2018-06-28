1a0bafd3955daac541680de4cbf37f482ae28e3b
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-28 13:34:10 +0200

    pequeño bug fix

--- commands/suicidio.js
+++ commands/suicidio.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) =>
 {
     //k-suicidar <Nota de suicidio>
     let nota = message.content;
-    message.delete();
+    message.delete().catch();
     if(!nota) return message.channel.send(`¡<@${message.author.id}> se ha suicidado 😮! DEP 💀 <@${message.author.id}> 💀`);
     if(nota) return message.channel.send(`¡<@${message.author.id}> se ha suicidado 😮!` +  " Nos ha dejado una nota: " + `**${nota}**` + ` DEP 💀 <@${message.author.id}> 💀`);
     
