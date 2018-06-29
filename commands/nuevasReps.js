ddeb59a706b55236188cda91a1d08d71c13b5660
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-29 17:16:58 +0200

    updated reps sot

--- commands/nuevasReps.js
+++ commands/nuevasReps.js
@@ -11,10 +11,10 @@ module.exports.run = async (bot, message, args) =>
         var repsSotEmbed = new Discord.RichEmbed()
         .setColor("#39efbf")
         .setTitle("Reputaciones Actuales")
-        .addField("EG Gamer", "**40**, **40**, **40**, **2**")
-        .addField("Hyren", "**40**, **41**, **39**, **2**")
+        .addField("EG Gamer", "**41**, **41**, **41**, **2**")
+        .addField("Hyren", "**40**, **41**, **40**, **2**")
         .addField("Fran", "**30**, **28**, **27**, **1**")
-        .addField("Carlis", "**?**, **?**, **?**, **0**")
+        .addField("Carlis", "**20**, **21**, **18**, **0**")
         .addField("Cutu", "**22**, **21**, **17**, **0**")
         .setImage("https://i.imgur.com/ypara7v.png")
         .setThumbnail("https://compass-ssl.xbox.com/assets/25/75/2575a893-7267-4ad5-ad44-3a4a439c3d0c.png?n=dt_hero_SOT_logo.png");
