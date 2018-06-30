01b026d4a1e85ab30c73e572cc7c0bf63132dfb5
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-30 15:57:53 +0200

    test

--- commands/chanclas.js
+++ commands/chanclas.js
@@ -4,25 +4,22 @@ const Chancla = require("../models/chancla.js");
 
 module.exports.run = async (bot, message, args) => {
     Mongoose.connect(process.env.MONGODB_URI);
-    let query = Chancla.where({userID: `${message.author.id}`})
-    if(!query){        
+   
+    let generatedCoinAmt = Math.floor(Math.random() * 15) + 1;
+   
+    Chancla.where({ userID: message.author.id}).update({ chanclaAmt: generatedCoinAmt});
+    
+    if(!Chancla.where({ userID: message.author.id})){        
     const chancla = new Chancla({
         //_id: Mongoose.Types.ObjectId(),
         username: message.author.username,
         userID: message.author.id,
-        chanclaAmt: 0
-        
+        chanclaAmt: generatedCoinAmt        
     });
-    }else if (query){
-        query.updateOne({ chanclaAmt: 5}, function (err, raw) {
-            if (err) return handleError(err);
-              console.log('The raw response from Mongo was ', raw);
-            });
-    
-        
-        
+
     }
     
+
     
     
     
