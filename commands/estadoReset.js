985b006def5305852ceb4dcea90da9296e93e848
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-01 21:32:04 +0200

    bug fix

--- commands/estadoReset.js
+++ commands/estadoReset.js
@@ -7,8 +7,6 @@ module.exports.run = async (bot, message, args) =>
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
     message.delete();
     bot.user.setActivity(`LOS KRUKEN CHANCLAS | ${botconfig.version}`, {type: "WATCHING"});
-  
-console.log(argus);
 
 }
 module.exports.help = {
