bec97ab95dacd96b285dcab6158f5d3227438873
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-01 21:29:17 +0200

    xd

--- commands/estadoReset.js
+++ commands/estadoReset.js
@@ -3,14 +3,14 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) =>
 {
-    let argus = args.join(" ");
+   
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
     message.delete();
-    bot.user.setActivity(`${argus} | ${botconfig.version}`);
+    bot.user.setActivity(`LOS KRUKEN CHANCLAS | ${botconfig.version}`, {type: "WATCHING"});
   
 console.log(argus);
 
 }
 module.exports.help = {
-    name: "status"
+    name: "statusReset"
 }
