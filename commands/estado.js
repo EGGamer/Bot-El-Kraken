04966aebec46bcde95b129a5abb2581e1d73880b
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-01 19:35:59 +0200

    añadido restart

--- commands/estado.js
+++ commands/estado.js
@@ -6,8 +6,11 @@ module.exports.run = async (bot, message, args) =>
     let argus = args.join(" ");
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
     message.delete();
+    if(argus = "restart"){
+        bot.user.setActivity(`LOS KRUKEN CHANCLAS | ${botconfig.version}`, {type: "WATCHING"});
+    }else {
     bot.user.setActivity(`${args} | ${botconfig.version}`);
-
+}
 }
 
 module.exports.help = {
