8a3f9faac88ca011dac47ee8741e68046e962441
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-06 21:33:32 +0200

    eliminado feature

--- commands/estado.js
+++ commands/estado.js
@@ -4,26 +4,11 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args) =>
 {
     let argus = args.join(" ");
-    let type = args[0];
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
-    message.delete();
     
-    if(type === undefined)
-    {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
+    message.delete();    
         bot.user.setActivity(`${argus} | ${botconfig.version}`);
-    }
-    if(type === "play"){ 
-        bot.user.setActivity(`${argus} | ${botconfig.version}`);
-    }
-    if(type === "watch"){ 
-        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "WATCHING"});
-    }
-    if(type === "listen"){ 
-        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "LISTENING"});
-    }
-    if(type === "stream"){ 
-        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "STREAMING"});
-    }
+
 console.log(argus);
 
 }
