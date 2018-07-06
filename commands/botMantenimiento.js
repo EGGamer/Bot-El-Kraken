9e5a21f02c4d49f5836dd13e7700087a21b6d2c0
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-06 21:35:42 +0200

    Bot mantenimiento

---
+++ commands/botMantenimiento.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>
+{ 
+    let author = message.member;
+  
+
+    if(!author.hasPermission("ADMINISTRATOR")) return message.reply("¡No puede ejecutar ese comando!");
+    if(author.hasPermission("ADMINISTRATOR")){
+    let anunciosChannel = message.guild.channels.find(`name`, "anuncios-server");
+    let embed = new Discord.RichEmbed()
+    .setColor("#f4e542")
+    .setTitle("BOT EN MANTENIMIENTO")
+    .setDescription("Estamos haciendo mantinimiento del bot y/o programando al mismo. Puede que esté caído por algunos momentos.")
+    .setThumbnail("https://cdn.discordapp.com/avatars/454682928769663007/14ac96f716c195bf55d7373778bd092c.png")
+    .setTimestamp();
+   
+    anunciosChannel.send(embed)
+    message.delete();
+}
+}
+
+module.exports.help = {
+    name: "botmantenimiento"
+}
