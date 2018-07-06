2c3f5abc7a045567a92f4d0c8e2ad82b131bb475
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-06 21:29:24 +0200

    otros tipos de estados

--- commands/estado.js
+++ commands/estado.js
@@ -3,11 +3,27 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) =>
 {
-    let argus = args.join(" ");
+    let argus = args[1].join(" ");
+    let type = args[0];
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No voy a dejar de hacer lo que estoy haciendo.");
     message.delete();
-    bot.user.setActivity(`${argus} | ${botconfig.version}`);
-  
+    
+    if(type === undefined)
+    {
+        bot.user.setActivity(`${argus} | ${botconfig.version}`);
+    }
+    if(type === "play"){ 
+        bot.user.setActivity(`${argus} | ${botconfig.version}`);
+    }
+    if(type === "watch"){ 
+        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "WATCHING"});
+    }
+    if(type === "listen"){ 
+        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "LISTENING"});
+    }
+    if(type === "stream"){ 
+        bot.user.setActivity(`${argus} | ${botconfig.version}`, {type: "STREAMING"});
+    }
 console.log(argus);
 
 }
