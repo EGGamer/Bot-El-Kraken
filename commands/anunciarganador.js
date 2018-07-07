66802949aa5886786c559f9f09b2c370802e3f29
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-07 19:29:48 +0200

    comando anuncio ganador

---
+++ commands/anunciarganador.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>
+{
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nope");
+    let User = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    message.delete();
+    let ganadorEmbed = new Discord.RichEmbed()
+    .setTitle("GANADOR DE EL MAESTRO DE LOS CHISTES")
+    .setDescription(`¡${User} ha ganado el concurso de **El Maestro de los Chistes**!`)
+    .setAuthor(User.username)
+    .setTimestamp()
+    .setFooter("¡Enhorabuena!")
+    .setColor("#54c0ff");
+    let anunciosChannel = message.guild.channels.find(`name`, "anuncios-server");
+    anunciosChannel.send(ganadorEmbed);
+}
+
+module.exports.help = {
+    name: "say"
+}
