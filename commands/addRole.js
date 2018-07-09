7d8a8eed6bcc87b5e21c7c6980c0f203d271f335
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-09 18:47:24 +0200

    debug

--- commands/addRole.js
+++ commands/addRole.js
@@ -6,11 +6,11 @@ module.exports.run = async (bot, message, args) =>
     let role = args;
     if(!role) return message.reply("¡Especifica un rol!");
     let testRojorol = message.guild.roles.find(`name`, "ROL TEST 1");
-    
+    console.log(testRojorol);
+    console.log(testRojorol.id);
     if(args === "rojo"){
-        if(rMember.roles.has(testRojorol.id)){ 
-        return message.reply("¡Ya tienes es rol!"); 
-    }else{ await(rMember.addRole(testRojorol.id));}
+        if(rMember.roles.has(testRojorol.id));
+        await(rMember.addRole(testRojorol.id));}
     }
 
     try {
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) =>
     message.channel.send(`<@${rMember}> tiene el rol <@${testRojorol.name}>. Tiene los DM's desactivados.`);
     
 }
-}
+
 
 module.exports.help = {
     name: "añadirrol"
