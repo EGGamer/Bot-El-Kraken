4af671f9f13a5e142aa057f8c627f498e6bfee97
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-09 18:23:41 +0200

    Test addrole

--- commands/addRole.js
+++ commands/addRole.js
@@ -5,14 +5,17 @@ module.exports.run = async (bot, message, args) =>
     let rMember = message.guild.member;
     let role = args;
     if(!role) return message.reply("¡Especifica un rol!");
-    let gRole = message.guild.roles.find(`name`, role);
-    if(!gRole) return message.reply("¡No he podido encontrar ese rol!");
+    let testRojorol = message.guild.roles.find(`name`, "ROL TEST 1");
+    
+    if(args === "rojo"){
+        if(rMember.roles.has(testRojorol.id)){ 
+        return message.reply("¡Ya tienes es rol!"); 
+    }else await(rMember.addRole(testRojorol.id));
+    }
 
-    if(rMember.roles.has(gRole.id));
-    await(rMember.addRole(gRole.id));
 
     try {
-        rMember.send(`Felicidades, te he otorgado el rol ${gRole.name}`)
+       await rMember.send(`Felicidades, te he otorgado el rol ${gRole.name}`)
     }catch(e){
     message.channel.send(`<@${rMember}> tiene el rol ${gRole.name}. Tiene los DM's desactivados.`);
     
