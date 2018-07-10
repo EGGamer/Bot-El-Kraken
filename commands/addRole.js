059b40e1d32a9ee90c8804b317ee2d852c28f288
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-07-10 16:52:15 +0200

    disabilitado

--- commands/addRole.js
+++ commands/addRole.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json")
 module.exports.run = async (bot, message, args) =>
-{
+{/*
     let member = message.member.user;
     let testRojorol = message.guild.roles.find(`name`, "ROL TEST 1");
     
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) =>
         member.addRole(testRojorol).catch(console.error);
     }
     
-  /*  try {
+    try {
        await member.send(`Felicidades, te he otorgado el rol ${testRojorol.name}`);
     }catch(e){
     message.channel.send(`${member} tiene el rol <@${testRojorol.name}>. Tiene los DM's desactivados.`);
