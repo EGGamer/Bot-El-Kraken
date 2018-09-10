12724253e9dc24a2b7d29943965585f4512ff7b9
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-09-10 19:56:20 +0200

    Añadidos cambios

--- commands/changelog.js
+++ commands/changelog.js
@@ -5,9 +5,9 @@ module.exports.run = async (bot, message, args) =>
 {
     let embed = new Discord.RichEmbed()
     .setTitle(`CAMBIOS DE LA VERSIÓN ${botconfig.version}`)
-    .addField("NUEVAS FUNCIONES", `1. Nuevo comando: cambios. Este comando se utilizará para saber cambios de la reciente versión del bot. (Para todos los usuarios)\n 2. Nuevo comando: prefix. Ahora podremos cambiar el prefijo del bot sin tener que acceder al código del bot. (Solo administradores)`)
-    .addBlankField()
-    .addField("BUG FIXES", `Ahora al subir de nivel que cambie de rol por ejemplo nivel 5, no saldrá el mensaje de subir de nivel 2 veces.\n Arreglos en el comando de chanclas.`);
+    .addField(`Mejora de Chanclas y XP`, `Los sistemas de experiencia y chanclas han sido trasladados a nuestra base de datos para mejor rendimiento.`)
+    //.addBlankField()
+    //.addField("BUG FIXES", `Ahora al subir de nivel que cambie de rol por ejemplo nivel 5, no saldrá el mensaje de subir de nivel 2 veces.\n Arreglos en el comando de chanclas.`);
 
     message.channel.send(embed);
 
